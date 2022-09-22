97c4caa9a2f369aef0073c4c70ae83134fbe5760
Pavel Romanov <3metil@mail.ru>
2022-09-22 15:34:31 +0300

    Card

--- src/pages/ProductItem.jsx
+++ src/pages/ProductItem.jsx
@@ -2,24 +2,54 @@ import React from 'react';
 import { useParams } from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { products } from '../menuList'
+import './Productitem.scss'
+import Header from '../header/Header.jsx'
+import {addProduct} from '../store/reducers/index'
+import {useDispatch} from 'react-redux'
+import {v4 as uuidv4} from 'uuid'
 
 let menu =  products
 const ProductItem = () => {
     
     const {params} = useParams()
-    const id = (params - 1)
-    console.log(menu)
-    console.log(id)
-    const item = products.filter(({ id }) => id === products.id)[1]
+    const menu = products.filter(products => products.id === params)
+    const dispatch = useDispatch() 
 
-    console.log({menu})
-    
-    return (
-        <div>
-           {menu.title}
-        </div>
+    const handleAddProduct = () => {
         
-        // console.log(prop)
+        let item = {
+        id: uuidv4(),
+        title: menu[0].title,
+        img: menu[0].img,
+        price: menu[0].price
+
+        }
+        dispatch(addProduct(item))
+        alert('Товар добавлен в корзину')
+
+
+    }
+    return (
+                <><Header />
+                <div className='card-container'>
+                <div className="card">
+                
+                <div className="card__preview-wrap">
+                <img className='card-preview' src={menu[0].img} alt="" />
+                </div>
+                <div className="card__text-wrap">
+                <h2 className="card__title">{menu[0].title}</h2>
+        
+                <p className="card__description">{menu[0].description}
+                
+                </p>
+                
+                <div className="card__add-wrap">
+                <div className="card__price">{menu[0].price} ₽/ {menu[0].weight}</div>
+               <button className="card__add-button" onClick={handleAddProduct}>Добавить в корзину</button>
+               </div>
+                </div></div></div></>
+        
     )
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
-// import {products} from '../menuList.js'
 import { createSlice } from '@reduxjs/toolkit'
-// import cartReducer from './index.js'
-// import store from './store'
+
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -20,20 +18,3 @@ const cartSlice = createSlice({
 export const { addproduct } = cartSlice.actions
 
 export default cartSlice.reducer
-
-// const initialState = {
-//     products,
-//     cart: []
-// }
-
-// export const reducer = (state = initialState, action) => {
-//     switch (action.type) {
-//         case 'ADD_CART':
-//             return {...state, cart: [...state.cart, state.cards.find(product => product.id === action.id)]}
-//         case 'REM_CART':
-//             return {...state, cart: state.cart.filter(product => product.id !== action.id)}
-//         default:
-//             return state
-
-//     }
-// }
